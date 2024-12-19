ffa1ca344695ebd2aae6875dba50facea86a90b5
ashab-k <ashabkhan2000@gmail.com>
2024-12-19 14:42:53 +0530

    db models created

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,14 +3,15 @@ package main
 import (
 	"fmt"
 	"html/template"
-	"log"
 	"net/http"
 	"strconv"
+
+	"github.com/ashab-k/snippetbox/pkg/models"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
+func (app *application)home(w http.ResponseWriter, r *http.Request) {
     if r.URL.Path != "/" {
-        http.NotFound(w, r)
+        app.notFound(w)
         return
     }
 
@@ -18,37 +19,58 @@ func home(w http.ResponseWriter, r *http.Request) {
 
 	ts, err := template.ParseFiles(files...)
 	if err != nil {
-		log.Printf(err.Error())
-		http.Error(w , "Internal Server Error", http.StatusInternalServerError)
+		app.serverError(w , err)
+		return
 	}
 
 	err = ts.Execute(w , nil)
 
 	if err != nil {
-		log.Println(err.Error())
-        http.Error(w, "Internal Server Error", 500)
+		app.serverError(w ,err)
 	}
 
 
     w.Write([]byte("Hello from Snippetbox"))
 }
 
-func showSnippet(w http.ResponseWriter, r *http.Request) {
+func (app *application)showSnippet(w http.ResponseWriter, r *http.Request) {
     id, err := strconv.Atoi(r.URL.Query().Get("id"))
     if err != nil || id < 1 {
-        http.NotFound(w, r)
+       app.notFound(w)
         return
     }
 
-    fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
+	s, err := app.snippets.Get(id)
+    if err == models.ErrNoRecord {
+        app.notFound(w)
+        return
+    } else if err != nil {
+        app.serverError(w, err)
+        return
+    }
+
+    // Write the snippet data as a plain-text HTTP response body.
+    fmt.Fprintf(w, "%v", s)
 }
 
-func createSnippet(w http.ResponseWriter, r *http.Request) {
+func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
     if r.Method != "POST" {
         w.Header().Set("Allow", "POST")
-        http.Error(w, "Method Not Allowed", 405)
+        app.clientError(w ,http.StatusMethodNotAllowed)
+        return
+    }
+	title := "O snail"
+    content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi Issa"
+    expires := "7"
+
+    // Pass the data to the SnippetModel.Insert() method, receiving the
+    // ID of the new record back.
+    id, err := app.snippets.Insert(title, content, expires)
+    if err != nil {
+        app.serverError(w, err)
         return
     }
 
-    w.Write([]byte("Create a new snippet..."))
+    // Redirect the user to the relevant page for the snippet.
+    http.Redirect(w, r, fmt.Sprintf("/snippet?id=%d", id), http.StatusSeeOther)
 }
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,36 +1,68 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/ashab-k/snippetbox/pkg/models/mysql"
+
+	_ "github.com/go-sql-driver/mysql"
 )
+
+type application  struct{
+	errLog *log.Logger
+	infoLog *log.Logger
+	snippets *mysql.SnippetModel
+}
 
 func main() {
 
 	addr := flag.String("addr" ,":4000" , "HTTP network port")
-    mux := http.NewServeMux()
-    mux.HandleFunc("/", home)
-    mux.HandleFunc("/snippet", showSnippet)
-	
-    mux.HandleFunc("/snippet/create", createSnippet)
-
-	fileServer := http.FileServer(http.Dir("./ui/static"))
-	mux.Handle("/static/" , http.StripPrefix("/static" , fileServer))
-
-
+	dsn := flag.String("dsn" , "web:anas0707@/snippetbox?parseTime=true" , "MySQL datasource name")
+	 
+	flag.Parse()
 	infoLog := log.New(os.Stdout , "INFO\t" , log.Ldate | log.Ltime)
 
 	errLog := log.New(os.Stderr , "ERROR\t" , log.Ldate|log.Ltime|log.Llongfile)
 
+
+	db , err := openDB(*dsn)
+	
+	if err != nil {
+		errLog.Fatal(err)
+	}
+
+	defer db.Close()
+	app := &application{
+		errLog: errLog,
+		infoLog: infoLog,
+		snippets: &mysql.SnippetModel{DB: db},
+	}
+
+  
+
     infoLog.Printf("Starting server on %s" , *addr)
 
-	srv := &http.Server{
-		Addr: *addr,
+	srv := &http.Server{ //we create our own instance of a server insead of relying on http.Server
+		Addr: *addr, 
 		ErrorLog: errLog,
-		Handler: mux,
+		Handler: app.routes(),
 	}
-    err := srv.ListenAndServe()
+    err = srv.ListenAndServe()
     errLog.Fatal(err)
+}
+
+func openDB(dsn string) (*sql.DB , error){
+	db , err := sql.Open("mysql" , dsn)
+	if err != nil {
+		return nil , err
+	}
+
+	if err = db.Ping() ; err != nil{
+		return nil , err
+	}
+	return db , nil 
 }
