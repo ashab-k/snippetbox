40d0e03854ad5586341d0be63bfe2970fae998ca
ashab-k <ashabkhan2000@gmail.com>
2024-12-22 00:28:40 +0530

    upto chapter 8

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -4,16 +4,13 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
+	"strings"
+	"unicode/utf8"
 
 	"github.com/ashab-k/snippetbox/pkg/models"
 )
 
 func (app *application)home(w http.ResponseWriter, r *http.Request) {
-    if r.URL.Path != "/" {
-        app.notFound(w)
-        return
-    }
-
 	s , err := app.snippets.Latest()
 	if err != nil {
 		app.serverError(w , err)
@@ -26,12 +23,11 @@ func (app *application)home(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application)showSnippet(w http.ResponseWriter, r *http.Request) {
-    id, err := strconv.Atoi(r.URL.Query().Get("id"))
+    id, err := strconv.Atoi(r.URL.Query().Get(":id"))
     if err != nil || id < 1 {
        app.notFound(w)
         return
     }
-
 
 	s, err := app.snippets.Get(id)
     if err == models.ErrNoRecord {
@@ -49,24 +45,66 @@ func (app *application)showSnippet(w http.ResponseWriter, r *http.Request) {
 	})
 }
 
+func (app *application) createSnippetForm(w http.ResponseWriter, r *http.Request) {
+    app.render(w,r , "create.page.tmpl" , nil)
+}
+
+
+
 func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
-    if r.Method != "POST" {
-        w.Header().Set("Allow", "POST")
-        app.clientError(w ,http.StatusMethodNotAllowed)
+
+  err := r.ParseForm()
+
+  if err != nil {
+	 app.clientError(w, http.StatusBadRequest)
+	 return
+	
+  }
+
+  title := r.PostForm.Get("title")
+  content := r.PostForm.Get("content")
+  expires := r.PostForm.Get("expires")
+
+  errors := make(map[string]string)
+
+    // Check that the title field is not blank and is not more than 100 characters
+    // long. If it fails either of those checks, add a message to the errors
+    // map using the field name as the key.
+    if strings.TrimSpace(title) == "" {
+        errors["title"] = "This field cannot be blank"
+    } else if utf8.RuneCountInString(title) > 100 {
+        errors["title"] = "This field is too long (maximum is 100 characters)"
+    }
+
+    // Check that the Content field isn't blank.
+    if strings.TrimSpace(content) == "" {
+        errors["content"] = "This field cannot be blank"
+    }
+
+    // Check the expires field isn't blank and matches one of the permitted
+    // values ("1", "7" or "365").
+    if strings.TrimSpace(expires) == "" {
+        errors["expires"] = "This field cannot be blank"
+    } else if expires != "365" && expires != "7" && expires != "1" {
+        errors["expires"] = "This field is invalid"
+    }
+
+    // If there are any errors, dump them in a plain text HTTP response and return
+    // from the handler.
+    if len(errors) > 0 {
+      app.render(w,r, "create.page.tmpl" , &templateData{
+		FormData: r.PostForm,
+		FormErrors: errors,
+	  })
         return
     }
-	title := "O snail"
-    content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi Issa"
-    expires := "7"
 
-    // Pass the data to the SnippetModel.Insert() method, receiving the
-    // ID of the new record back.
     id, err := app.snippets.Insert(title, content, expires)
     if err != nil {
         app.serverError(w, err)
         return
     }
-
-    // Redirect the user to the relevant page for the snippet.
-    http.Redirect(w, r, fmt.Sprintf("/snippet?id=%d", id), http.StatusSeeOther)
+    // Change the redirect to use the new semantic URL style of /snippet/:id
+    http.Redirect(w, r, fmt.Sprintf("/snippet/%d", id), http.StatusSeeOther)
 }
+
